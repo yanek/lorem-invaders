6a177d8ee6386ae90ee871773e6bbeacc334937f
Noé Ksiazek <noe@ksiazek.xyz>
2024-09-21 16:47:41 +0200

    Version bump

--- src/utils.h
+++ src/utils.h
@@ -3,10 +3,6 @@
 #include <string>
 #include <vector>
 
-#define VERSION_MAJOR 0
-#define VERSION_MINOR 5
-#define VERSION_PATCH 1
-
 #define E_BAIL(val, ...)              \
 	TraceLog(LOG_ERROR, __VA_ARGS__); \
 	return val;
